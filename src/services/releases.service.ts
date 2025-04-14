3a569a7f8c3244faebf30d792a44e8d84cd0b101
kibni1409 <kibni1409@gmail.com>
2025-04-14 19:20:35 +0300

    fix apiv2

--- src/services/releases.service.ts
+++ src/services/releases.service.ts
@@ -1,10 +1,15 @@
-import {ApiResponse} from '@/types';
-import {apiClient} from '@/services/api/client';
+import axios from 'axios';
+import { ApiResponse } from '@/types';
 import {TDataRelease, TRelease} from "@/services/types";
+
+const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL_API,
+    timeout: 10000
+});
 
 export const ReleaseService = {
     async getReleases(page: number = 1, limit: number = 15): Promise<ApiResponse<TRelease>> {
-        return apiClient.get<ApiResponse<TRelease>>('/anime/catalog/releases', {
+        const { data } = await api.get<ApiResponse<TRelease>>('/anime/catalog/releases', {
             params: {
                 page,
                 limit,
@@ -12,13 +17,16 @@ export const ReleaseService = {
                 'f[years][from_year]': 1990
             }
         });
+        return data;
     },
 
     async getDataRelease(name: string): Promise<TDataRelease> {
-        return apiClient.get<TDataRelease>(`/anime/releases/${name}`);
+        const { data } = await api.get<TDataRelease>(`/anime/releases/${name}`);
+        return data;
     },
 
     async getSearch(text: string): Promise<TRelease[]> {
-        return apiClient.get<TRelease[]>(`app/search/releases/?query=${text}`)
+        const { data } = await api.get<TRelease[]>(`app/search/releases/?query=${text}`);
+        return data;
     }
 };
