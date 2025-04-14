d9f51842dd91cb2fe19d429ca380ed9a7cfb56da
kibni1409 <kibni1409@gmail.com>
2025-04-14 17:48:29 +0300

    fix config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
-      domains: ['anilibria.top', 'anilibria.topnull'],
+    domains: ['anilibria.top', 'anilibria.topnull'],
+    unoptimized: true
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production'
   }
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
